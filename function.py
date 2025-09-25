37b30dee381c20ca8cbd1621b21ad76c7ce99c22
Rayman12393 <rayanenser@hotmail.com>
2025-09-25 19:30:25 +0200

    feat: implémentation de la classe Tetris
    Mise en place de la classe principale Tetris qui initialise la grille, la pièce active, la position de départ, le score et l’état du jeu.

--- function.py
+++ function.py
@@ -21,3 +21,12 @@ PIECES = [
     [[0, 1, 1], [1, 1, 0]],  # S
     [[1, 1, 0], [0, 1, 1]]   # Z
 ]
+class Tetris:
+    def __init__(self):
+        self.grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+        self.piece = random.choice(PIECES)
+        self.piece_x = GRID_WIDTH // 2 - len(self.piece[0]) // 2
+        self.piece_y = 0
+        self.score = 0
+        self.game_over = False
+        self.running = True
