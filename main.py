d21567b7719c6887e597bf0e63b6307efa3ac04f
Rayman12393 <rayanenser@hotmail.com>
2025-09-25 19:03:49 +0200

    feat: ajout de la structure du projet et des dépendances

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Tetris Console - Version Python
+Jeu de Tetris jouable en console avec des pièces 3x3 sur une grille 10x10.
+"""
+import time
+from functions import TetrisGame, print_game, get_input, handle_input
